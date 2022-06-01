8a77ad2370b04d69e0afa294468469cefbc107c5
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-06-01 10:50:42 +0600

    table correction done

--- src/Components/UserPages/ManageItems/ManageItems.js
+++ src/Components/UserPages/ManageItems/ManageItems.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table'
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { BsTrashFill } from "react-icons/bs";
 import { toast } from 'react-toastify';
 import useProducts from '../../Utilities/Hook/AllproductHook';
@@ -24,24 +24,32 @@ const ManageItems = () => {
     return (
         <div className='m-5'>
             <h4 className='text-center pb-3'>Remove item from here</h4>
-            {
-                products.map(product =>
-                    <div key={product._id}>
-                        <Table className='' striped bordered hover>
-                            <tbody>
-                                <tr>
-                                    <td></td>
-                                    <td>{product.name}</td>
-                                    <td>{product.suppliar}</td>
-                                    <td className='text-center'><button onClick={() => handleDelete(product._id)} className='text-danger border-0'>Delete <BsTrashFill /></button></td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </div>)
-            }
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Name</th>
+                        <th>Suppliar Name</th>
+                        <th>Modify</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) =>
+                            <tr key={product._id}>
+                                <td>{index + 1}</td>
+                                <td>{product.name}</td>
+                                <td>{product.suppliar}</td>
+                                <td className='text-center'><button onClick={() => handleDelete(product._id)} className='text-danger border-0'>Delete <BsTrashFill /></button></td>
+                            </tr>
+                        )
+                    }
+
+                </tbody>
+            </Table>
 
             <div className='d-flex justify-content-center py-2'>
-                <Link to='/additems'><button style={{background: '#007CC3'}} className='text-white border-0 py-2 px-5'>Add New Item</button></Link>
+                <Link to='/additems'><button style={{ background: '#007CC3' }} className='text-white border-0 py-2 px-5'>Add New Item</button></Link>
             </div>
 
         </div>
